c6cc6a601fadf33b4d4a9f6a8e07133cb4bad247
Philip Steingrüber <philip.steingruber@gmail.com>
2024-11-28 12:49:56 +0100

    Moved layout components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
+import Footer from "@/components/Layout/Footer";
+import Navbar from "@/components/Layout/Navbar";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import MaxWidthWrapper from "./MaxWidthWrapper";
+import MaxWidthWrapper from "../MaxWidthWrapper";
 
 export default function Footer() {
   return (
