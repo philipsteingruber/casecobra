8bf194f42f264eefc676786e02ad4193a9b75051
Philip Steingrüber <philip.steingruber@gmail.com>
2024-12-01 19:53:32 +0100

    Logic for uploading images to db

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,3 +1,11 @@
-export default async function DesignPage() {
-  return <div></div>;
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+export default async function DesignPage({ searchParams }: PageProps) {
+  const { id } = searchParams;
+
+  return <div>{id}</div>;
 }
