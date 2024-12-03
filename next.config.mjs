4fd3a8ea46dbe2caff8df6d0a0f1f20f9b9c6a34
PhilipSteingrüber <philip.steingruber@gmail.com>
2024-12-03 14:45:37 +0100

    Updated nextjs config to remotepatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["utfs.io"],
+    remotePatterns: {
+      hostname: "utfs.io",
+      protocol: "https",
+    },
   },
 };
 
