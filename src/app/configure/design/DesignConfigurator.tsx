6331bfe758084ff0df6f6c01a229c3d135562b26
PhilipSteingrüber <philip.steingruber@gmail.com>
2024-12-03 12:40:00 +0100

    DesignConfigurator layout responsiveness

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -44,9 +44,8 @@ export default function DesignConfigurator({
     material: MATERIALS.selectableOptions[0],
     finish: FINISHES.selectableOptions[0],
   });
-
   return (
-    <div className="relative mb-20 mt-20 grid grid-cols-3 pb-20">
+    <div className="relative mb-20 mt-20 grid grid-cols-1 pb-20 lg:grid-cols-3">
       <div className="relative col-span-2 flex h-[37.5rem] w-full max-w-4xl items-center justify-center overflow-hidden rounded-lg border-2 border-dashed border-gray-300 p-2 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
         <div className="pointer-events-none relative aspect-[896/1831] w-60 bg-opacity-50">
           <AspectRatio
@@ -95,7 +94,7 @@ export default function DesignConfigurator({
           </div>
         </Rnd>
       </div>
-      <div className="flex h-[37.5rem] flex-col bg-white">
+      <div className="col-span-full flex h-[37.5rem] w-full flex-col bg-white lg:col-span-1">
         <ScrollArea className="relative flex-1 overflow-auto">
           <div
             aria-hidden="true"
